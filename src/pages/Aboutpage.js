546861b500fd011c72a5067486f71501b7f1190c
unknown <marose5388@gmail.com>
2021-07-22 17:11:22 -0500

    updated about page

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -1,7 +1,30 @@
+import Aboutimage from '../about-image.jpeg'
+
 function Aboutpage() {
   return (
-    <div>
-      <h1>Is it working</h1>
+    <div className='about-container'>
+      <div className='about-image-container'>
+        <img className='about-image' src={Aboutimage} alt='about the gym' />
+      </div>
+      <div className='about-text-container'>
+        <h1>About Us</h1>
+        <div className='about-text-columns'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus laudantium dolores, optio in, hic tempore quam minus
+            quae minima praesentium velit ex beatae ipsum perspiciatis nobis
+            quaerat odit quo consequuntur soluta modi. Dolorem maiores corporis
+            accusantium illum laudantium similique ducimus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
+            voluptate ex qui voluptatibus nostrum, doloribus praesentium
+            quibusdam id. Cum debitis commodi sapiente nostrum ad possimus
+            aperiam? Vero eius recusandae alias quam repudiandae. Nobis, quasi
+            iure?
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,14 @@
+import Image from '../gym-main-image.jpeg'
 function Homepage() {
   return (
     <div className='homepage-container'>
-      <h1 className='page-title homepage-title'>Energy Stadium</h1>
-      <p className='homepage-text'>The energy here will change your life!</p>
+      <div className='homepage-column-text'>
+        <h1 className='page-title homepage-title'>Energy Stadium</h1>
+        <p className='homepage-text'>The energy here will change your life!</p>
+      </div>
+      <div className='homepage-column-image'>
+        <img className='homepage-image' src={Image} alt='man doing pullup' />
+      </div>
     </div>
   )
 }
